e2a3f93308066d6ed2ad12fd8dc06a73e43bdbae
sinsia27 <syysia2787@gmail.com>
2026-04-17 10:19:58 +0900

    [level 0] Title: 홀짝 구분하기, Time: 18.11 ms, Memory: 7.48 MB -BaekjoonHub

---
+++ 프로그래머스/level_0/Basic/Day2_홀짝 구분하기/홀짝 구분하기.py
@@ -0,0 +1,15 @@
+a = int(input())
+
+print(a, "is even" if a % 2 == 0 else "is odd")
+
+# print(f"{a} is {'even' if a % 2 == 0 else 'odd'}")
+
+# if a % 2 == 0:
+#     print(f"{a} is even")
+# else :
+#     print(f"{a} is odd")
+    
+# if a % 2 == 0 :
+#     print( a, 'is even')
+# else :
+#     print( a, 'is odd')
