979964be963309f980da6f065eb806f28fb6b87d
sinsia27 <syysia2787@gmail.com>
2026-04-17 10:17:00 +0900

    [level 0] Title: a와 b 출력하기, Time: 17.78 ms, Memory: 7.71 MB -BaekjoonHub

---
+++ 프로그래머스/level_0/Basic/Day1_a와 b 출력하기/a와 b 출력하기.py
@@ -0,0 +1,14 @@
+a, b = map(int, input().strip().split(' '))
+
+print("a = " + str(a) + "\nb = " + str(b))
+
+# print("a =", a, "\nb =", b)
+
+# print("a =", a)
+# print("b =", b)
+
+# print(f"a = {a}\nb = {b}")
+
+# print("a = " + format(a) + "\nb = " + format(b))
+
+#f-string 문자열 문법
